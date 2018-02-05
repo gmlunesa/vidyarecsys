9d69ea3f3634f21e016534c1815bc8c3a9d9dc31
Goldy Mariz Lunesa <gmlunesa@gmail.com>
2018-02-05 17:48:19 +0800

    Writes to CSV

--- preprocessing/generatematrix.py
+++ preprocessing/generatematrix.py
@@ -4,7 +4,7 @@ import csv
 import json
 
 # Retrieve file names in the directory
-file_list = os.listdir("data/")
+file_list = os.listdir("dataset/")
 
 with open("applist_genre.json", encoding="utf8") as applist:
   applist_data = json.load(applist)
@@ -12,51 +12,76 @@ with open("applist_genre.json", encoding="utf8") as applist:
 # Loop through each json file in the directory
 for file_name in file_list:
 
-	# Action 				[0]
-	# Strategy 				[1]
-	# RPG					[2]
-	# Indie 				[3]
-	# Adventure 			[4]
-	# Sports				[5]
-	# Simulation			[6]
-	# Early Access 			[7]
-	# Ex Early Access 		[8]
-	# MMO 					[9]
-	# Free 					[10]
+	# Action 				[1] 1
+	# Strategy 				[2] 2
+	# RPG					[3] 3
+	# Indie 				[4] 23
+	# Adventure 			[5] 25
+	# Sports				[6] 18
+	# Simulation			[7] 28
+	# MMO		 			[8] 29
+	# Free 			 		[9] 37
+	# Casual 				[10] 4
 
 	# Make an array of corresponding features
-	genre_features = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	genre_features = [file_name[:-5], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 	# Open the file indicated by the current file name
-	with open("data/" + file_name, encoding="utf8") as src_file:
-	    data = json.load(src_file)
+	if file_name != ".DS_Store":
+		with open("dataset/" + file_name) as src_file:
+		    data = json.load(src_file)
 
-	if data is not None:
-		if data["response"] is not None:
-			# Traverse through each game
-			for item in data["response"]["games"]:
-				for genre in applist_data[str(item["appid"])]["genres"]:
+		if data is not None:
+			if data["response"] is not None:
+				if data["response"]["games"] is not None:
+					# Traverse through each game owned by the user
+					for item in data["response"]["games"]:
 
-					# To Do: Investigate what indices do the genres have via steam spy https://steamdb.info/genres/
-					print(genre["id"])
+						print(item["appid"])
 
-					genre_features[0] = genre_features[0] + 1
+						# if genre list contains the game item
+						if applist_data[str(item["appid"])] is not None:
 
-					print(genre_features)
+							# Loop through the genres of the said item
+							for genres in applist_data[str(item["appid"])]:
 
-					
+								if genres["id"] is not None:
+									if genres["id"] == '1':
+										genre_features[1] = genre_features[1] + 1
+									elif genres["id"] == '2':
+										genre_features[2] = genre_features[2] + 1
+									elif genres["id"] == '3':
+										genre_features[3] = genre_features[3] + 1
+									elif genres["id"] == '23':
+										genre_features[4] = genre_features[4] + 1
+									elif genres["id"] == '25':
+										genre_features[5] = genre_features[5] + 1
+									elif genres["id"] == '18':
+										genre_features[6] = genre_features[6] + 1
+									elif genres["id"] == '28':
+										genre_features[7] = genre_features[7] + 1
+									elif genres["id"] == '29':
+										genre_features[8] = genre_features[8] + 1
+									elif genres["id"] == '37':
+										genre_features[9] = genre_features[9] + 1
+									elif genres["id"] == '4':
+										genre_features[10] = genre_features[10] + 1
 
+									print(genre_features)
 
+						
+		
 
-# loop through each json file in the directory
-	# loop through go on ["response"]["games"]
-		# access ["response"]["games"] genre id and then cross reference it to the general app list
-		# increment the genre category of that person
-		#
-		#
-
-# with open('eggs.csv', 'w', newline='') as csvfile:
-#     spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-#     spamwriter.writerow(['Spam', 'Baked Beans'])
-# #     spamwriter.writerow(['Spam',
-#  'Lovely Spam', 'Wonderful Spam'])
+		# Print to CSV file tbh	
+		with open('matrixdata.csv', 'a', newline='') as csvfile:				
+			csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+			csvwriter.writerow([genre_features[0], str(genre_features[1]), 
+				str(genre_features[2]),
+				str(genre_features[3]),
+				str(genre_features[4]),
+				str(genre_features[5]),
+				str(genre_features[6]),
+				str(genre_features[7]),
+				str(genre_features[8]),
+				str(genre_features[9]),
+				str(genre_features[10])])	
